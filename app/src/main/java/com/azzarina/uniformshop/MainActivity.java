a6deafd85f7e01b217ceb71633c55159c555f8da
azzarina <azzarinaazizi@gmail.com>
2021-06-17 10:36:01 +1200

    logo and app heading

--- app/src/main/java/com/azzarina/uniformshop/MainActivity.java
+++ app/src/main/java/com/azzarina/uniformshop/MainActivity.java
@@ -3,16 +3,24 @@ package com.azzarina.uniformshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView bg_slidehshow = findViewById(R.id.bg_slideshow);
+        AnimationDrawable animationDrawable = (AnimationDrawable) bg_slidehshow.getDrawable();
+        animationDrawable.start();
     }
 
     public void sendMessage(View aView) {
@@ -24,4 +32,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, buy001.class);
         startActivity(intent);
     }
+
+    public void sendMessage2(View aView) {
+        Intent intent = new Intent(this, uniformGuide.class);
+        startActivity(intent);
+    }
 }
+
+
+
---
+++ app/src/main/java/com/azzarina/uniformshop/uniformGuide.java
@@ -0,0 +1,14 @@
+package com.azzarina.uniformshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uniformGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uniform_guide);
+    }
+}
